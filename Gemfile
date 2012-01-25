be9fc3c28ac1fa39b4a78b5a845fad4afb9dd2c4
James Sadler <freshtonic@gmail.com>
2012-01-25 19:20:14 +1100

    Can now open all of the files that are passed in

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ source :rubygems
 
 #gemspec
 gem 'trollop', '1.16.2'
+gem 'pry'
 
 group :test do
   gem 'rspec', '2.6'
-  gem 'pry'
 end
--- lib/csvmagic.rb
+++ lib/csvmagic.rb
@@ -1,4 +1,5 @@
 require 'trollop'
+require 'pry'
 
 module CSVMagic
   class Command
@@ -39,117 +40,118 @@ module CSVMagic
 
         opt :exclude, 
 
-          "Any rows matching this Ruby expression will be excluded from the
+          Command.nice("Any rows matching this Ruby expression will be excluded from the
           output. Can be specified multiple times and will be ORed together.
-          (cannot use --include with --exclude).", 
+          (cannot use --include with --exclude)."), 
 
           :type => String, :multi => true
 
         opt :include, 
 
-          "Any rows not matching this Ruby expression will be excluded from the
+          Command.nice("Any rows not matching this Ruby expression will be excluded from the
           output. Can be specified multiple times and will be ORed together.
-          (cannot use --include with --exclude). ", 
+          (cannot use --include with --exclude). "), 
           
           :type => String, :multi => true
 
         opt :save_excluded, 
 
-          "Any rows excluded from the output will be saved to the file. The row
-          will be preserved in its original form", 
+          Command.nice("Any rows excluded from the output will be saved to the file. The row
+          will be preserved in its original form"), 
           
           :type => String
 
         opt :select, 
           
-          "Comma-seperated list of columns to output. Each item in the list is
-          a Ruby expression", 
+          Command.nice("Comma-seperated list of columns to output. Each item in the list is
+          a Ruby expression"), 
           
           :type => String
 
         opt :in_place, 
           
-          "Modify the input file in place. WARNING: this operation is
-          destrcutive! (Does not apply when reading from STDIN)", 
+          Command.nice("Modify the input file in place. WARNING: this operation is
+          destrcutive! (Does not apply when reading from STDIN)"), 
           
           :default => false
 
         opt :header, 
           
-          "Treat the first line of the CSV file as a header that contains the
+          Command.nice("Treat the first line of the CSV file as a header that contains the
           column names. You can now refer to columns by name in your
-          expressions", 
+          expressions"), 
           
           :default => false
 
         opt :headings, 
           
-          "Tells CSVMagic what the column names are so you can use the names in
-          your expressions. Not necessary if you use the --header option", 
+          Command.nice("Tells CSVMagic what the column names are so you can use the names in
+          your expressions. Not necessary if you use the --header option"), 
           
           :type => String
 
         opt :output_header, 
           
-          "Output the column names for all selected columns", 
+          Command.nice("Output the column names for all selected columns"), 
           
           :default => false
 
         opt :input_encoding, 
           
-          "The input encoding, e.g. ascii, UTF-8", 
+          Command.nice("The input encoding, e.g. ascii, UTF-8"), 
           
           :type => String
 
         opt :output_encoding, 
           
-          "The output encoding, e.g. ascii, UTF-8", 
+          Command.nice("The output encoding, e.g. ascii, UTF-8"), 
           
           :type => String
 
         opt :input_delimiter, 
           
-          "Column delimiter for the input", 
+          Command.nice("Column delimiter for the input"), 
           
           :type => String, :default => ','
 
         opt :output_delimiter, 
           
-          "Column delimiter for the output", 
+          Command.nice("Column delimiter for the output"), 
           
           :type => String, :default => ','
 
         opt :input_quotechar, 
           
-          "Input quote character", 
+          Command.nice("Input quote character"), 
           
           :type => String, :default => '"'
 
         opt :output_quotechar, 
           
-          "Output quote character", 
+          Command.nice("Output quote character"), 
           
           :type => String, :default => '"'
 
         opt :require, 
           
-          "Ruby files to require before processing. This enables the use of
+          Command.nice("Ruby files to require before processing. This enables the use of
           custom Ruby code to be called during expression evaluation. This
-          option can be used multiple times", 
+          option can be used multiple times"), 
           
           :type => String, :multi => true
 
         opt :eval, 
           
-          "Ruby code to be evaluated before processing. Use this option to
+          Command.nice("Ruby code to be evaluated before processing. Use this option to
           specify methods inline to be called during expression evaluation. This
-          option can be used multiple times", 
+          option can be used multiple times"), 
           
           :type => String, :multi => true
 
       end
 
-      if opts[:include] && opts[:exclude]
+
+      if opts[:include].count > 0 && opts[:exclude].count > 0
         Trollop::die :exclude, "--exclude cannot be used with --include" 
       end
 
@@ -157,6 +159,13 @@ module CSVMagic
         Trollop::die :header, "--header cannot be used with --headings" 
       end
 
+      Munger.new(opts, ARGV).process
+
+    end
+
+    def self.nice(s)
+      s.gsub(/[ \t\n]+/, ' ')
     end
   end
+
 end
